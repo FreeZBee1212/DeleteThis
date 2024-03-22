e03cb27c2dded048f17dce36358a88d100e3c751
FreeZBee1212 <warcraft.creater@mail.ru>
2024-03-22 10:06:45 +0300

    Change last commit

--- main/java/web/service/CarServiceImpl.java
+++ main/java/web/service/CarServiceImpl.java
@@ -10,8 +10,9 @@ import java.util.List;
 public class CarServiceImpl implements CarService {
 
     private long count;
+
     @Override
-    public List<Car> createCars(){
+    public List<Car> createCars() {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("Toyota", "Red", 3, ++count));
         cars.add(new Car("BMW", "Green", 5, ++count));
@@ -20,40 +21,10 @@ public class CarServiceImpl implements CarService {
         cars.add(new Car("Lada", "Black", 4, ++count));
         return cars;
     }
+
     @Override
     public List<Car> getCars(long count) {
         List<Car> listCars = createCars().stream().limit(count).toList();
-
-
-        // Исправил ужас ниже
-
-
-//        Car car1 = new Car("Toyota", "Red", 3);
-//        Car car2 = new Car("BMW", "Green", 5);
-//        Car car3 = new Car("Audi", "Black", 8);
-//        Car car4 = new Car("Kia", "White", 10);
-//        Car car5 = new Car("Lada", "Black", 4);
-//        if (count == 1) {
-//            list.add(car1);
-//        } else if (count == 2) {
-//            list.add(car1);
-//            list.add(car2);
-//        } else if (count == 3) {
-//            list.add(car1);
-//            list.add(car2);
-//            list.add(car3);
-//        } else if (count == 4) {
-//            list.add(car1);
-//            list.add(car2);
-//            list.add(car3);
-//            list.add(car4);
-//        } else if (count >= 5) {
-//            list.add(car1);
-//            list.add(car2);
-//            list.add(car3);
-//            list.add(car4);
-//            list.add(car5);
-//        }
         return listCars;
     }
 }
